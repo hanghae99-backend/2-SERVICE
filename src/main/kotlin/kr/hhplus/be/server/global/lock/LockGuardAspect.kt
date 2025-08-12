6a2a2dbba96f58b650842cee67d69976a1fa3e05
Ahnyeongjun <toinin@naver.com>
2025-08-12 19:44:49 +0900

    fix: order로 분산락 aop 먼저 실행되도록 수정

--- src/main/kotlin/kr/hhplus/be/server/global/lock/LockGuardAspect.kt
+++ src/main/kotlin/kr/hhplus/be/server/global/lock/LockGuardAspect.kt
@@ -5,6 +5,7 @@ import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.reflect.MethodSignature
 import org.slf4j.LoggerFactory
+import org.springframework.core.annotation.Order
 import org.springframework.expression.ExpressionParser
 import org.springframework.expression.spel.standard.SpelExpressionParser
 import org.springframework.expression.spel.support.StandardEvaluationContext
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component
 
 @Aspect
 @Component
+@Order(1)
 class LockGuardAspect(
     private val distributedLock: DistributedLock
 ) {
